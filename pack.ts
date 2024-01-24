ad97007b912a68832177a6e3ce83cdf676983c40
Chance Snow <git@chancesnow.me>
2024-01-24 03:07:55 -0600

    Initial commit

---
+++ pack.ts
@@ -0,0 +1,8 @@
+import * as coda from "@codahq/packs-sdk";
+
+// TODO: https://coda.io/p/27155?section=listing
+// TODO: https://coda.io/packs/build/latest/tutorials/get-started/cli/#add-code-to-the-pack
+export const pack = coda.newPack();
+
+// TODO: https://firebase.google.com/docs/reference/js
+
